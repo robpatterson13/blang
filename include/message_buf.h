a97ccebaa993809172c558ede8b26315e66520ce
robpatterson13 <robertpatterson1313@gmail.com>
2026-05-12 18:08:47 -0400

    Fixed clang errors

--- include/message_buf.h
+++ include/message_buf.h
@@ -11,13 +11,13 @@ struct message_buf {
 
 #define DEFINE_MSG_BUF(name) struct message_buf name = {0}
 
-inline bool msg_full(struct message_buf *buf) {
+static inline bool msg_full(struct message_buf *buf) {
   return buf->idx == MSG_BUF_SIZE;
 }
 
-inline bool msg_empty(struct message_buf *buf) { return buf->idx == 0; }
+static inline bool msg_empty(struct message_buf *buf) { return buf->idx == 0; }
 
-inline bool push_char(struct message_buf *buf, const char c) {
+static inline bool push_char(struct message_buf *buf, const char c) {
   if (msg_full(buf))
     return false;
 
@@ -25,7 +25,7 @@ inline bool push_char(struct message_buf *buf, const char c) {
   return true;
 }
 
-inline void clear_buf(struct message_buf *buf) {
-  memset(buf->buf, 0, sizeof(buf->buf));
+static inline void clear_buf(struct message_buf *buf) {
+  memset(buf->buf, 0, MSG_BUF_SIZE);
   buf->idx = 0;
 }
