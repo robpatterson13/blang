ab58b839decd641314c7fdfe0af11fb541df71b2
robpatterson13 <robertpatterson1313@gmail.com>
2026-05-12 22:33:12 -0400

    Updated mappings

--- include/opcode_mappings.h
+++ include/opcode_mappings.h
@@ -1,8 +1,30 @@
-#define ADD_WORD
-#define SUB_WORD
-#define DOUBLE_WORD
-#define HALVE_WORD
-#define SLEEP_WORD
-#define COMMIT_WORD
-#define FLUSH_WORD
-#define HALT_WORD
+#include <_static_assert.h>
+#include <string.h>
+
+#define MAX_WORD_SIZE 32
+
+// update these for local setup
+#define ADD1_WORD "add1"
+#define ADD5_WORD "add5"
+#define ADD10_WORD "add10"
+#define SUB1_WORD "sub1"
+#define SUB5_WORD "sub5"
+#define SUB10_WORD "sub10"
+#define SLEEP_WORD "sleep"
+#define COMMIT_WORD "commit"
+#define FLUSH_WORD "flush"
+#define HALT_WORD "halt"
+
+#define CHECK_LEN(str) \
+  static_assert(strlen(str) <= MAX_WORD_SIZE, "string for " #str " is too long")
+
+CHECK_LEN(ADD1_WORD);
+CHECK_LEN(ADD5_WORD);
+CHECK_LEN(ADD10_WORD);
+CHECK_LEN(SUB1_WORD);
+CHECK_LEN(SUB5_WORD);
+CHECK_LEN(SUB10_WORD);
+CHECK_LEN(SLEEP_WORD);
+CHECK_LEN(COMMIT_WORD);
+CHECK_LEN(FLUSH_WORD);
+CHECK_LEN(HALT_WORD);
